9ede8682f70da07dc3fe31a3b163568a5e84f925
SriSomanaath <govindalapudisrinath@gmail.com>
2024-01-09 19:52:24 +0530

    adj commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Srinath
+      <div className="container max-auto flex justify-between py-5 ">
+        <div>
+          <button>
+          Add Employee
+        </button>
+        </div>
+      </div>
     </main>
   )
 }
